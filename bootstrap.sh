6597c385816f00f5c0ac4455c0ff93b257dd6272
Artem Nizamiiev <upuyayua@gmail.com>
2025-11-24 23:52:21 +0200

    Solved2

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,14 @@ kubectl create ns mysql
 helm upgrade --install todoapp . -n todoapp
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl taint nodes kind-worker app=mysql:NoSchedule --overwrite
+kubectl taint nodes kind-worker2 app=mysql:NoSchedule --overwrite
+
+helm dependency update .infrastructure/helm-chart/todoapp
+
+helm upgrade --install todoapp .infrastructure/helm-chart/todoapp -n todoapp
+
+PROJECT_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+
+kubectl get all,cm,secret,ing -A > "$PROJECT_ROOT/output.log"
